ce12ae689f98b31ca6127499fae98ac5b9a6d203
Filipe Oliveira <fmo.oliveira@gmail.com>
2022-08-23 19:59:31 +0100

    23082022

--- p2.py
+++ p2.py
@@ -5,11 +5,21 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
-# n=3
-# sum=0
-# while n < 4000000:
-#     if n % 2 == 0:
-#         sum += n
-#     n = fibonacci(n)
-#
-#print(fibonacci(10))
+
+# sum = 0
+# term = 1
+
+# while term < 4000000:
+#     if term % 2 == 0:
+#         sum += term
+#     term = fibonacci(term)
+i=1
+sum=0
+n=0
+while n < 4000000:
+    n = fibonacci(i)
+    if n % 2 == 0:
+        sum += n
+    i += 1
+
+print(sum)
